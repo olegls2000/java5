01958d9404c7c12b895d2c98d77b433a54f83703
Oleg Pasichnyk <oleg.pasichnyk@kuehne-nagel.com>
2021-10-04 21:05:13 +0300

    Lesson 8

--- src/model/Point.java
+++ src/model/Point.java
@@ -8,19 +8,15 @@ public class Point {
         this.x = x;
         this.y = y;
     }
-
     public int getX() {
         return x;
     }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
     public int getY() {
         return y;
     }
-
+    public void setX(int x) {
+        this.x = x;
+    }
     public void setY(int y) {
         this.y = y;
     }
---
+++ src/model/Triangle.java
@@ -0,0 +1,55 @@
+package model;
+
+public class Triangle {
+    private Point a;
+    private Point b;
+    private Point c;
+
+    public Triangle(Point one, Point two, Point three) {
+        pointOneLineCheck(one, two, three);
+        this.a = one;
+        this.b = two;
+        this.c = three;
+    }
+
+    public Point getA() {
+        return a;
+    }
+
+    public void setA(Point a) {
+        pointOneLineCheck(a, this.b, this.c);
+        this.a = a;
+    }
+
+    public Point getB() {
+        return b;
+    }
+
+    public void setB(Point b) {
+        pointOneLineCheck(this.a, b, this.c);
+        this.b = b;
+    }
+
+    public Point getC() {
+        return c;
+    }
+
+    public void setC(Point c) {
+        pointOneLineCheck(this.a, this.b, c);
+        this.c = c;
+    }
+
+    public double calculatePerimeter() {
+
+        return 0.0d;
+    }
+
+    private void pointOneLineCheck(Point one, Point two, Point three) {
+        double lefPart = (three.getY() - one.getY()) / (two.getY() - one.getY());
+        double rightPart = (three.getX() - one.getX()) / (two.getX() - one.getX());
+        if (lefPart == rightPart) {
+            System.out.println("Triangle impossible!!");
+            throw new RuntimeException();
+        }
+    }
+}
