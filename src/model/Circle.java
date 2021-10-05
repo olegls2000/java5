c3f84bb40f96fc206976f69d398b44f7c842daec
macbook <vladimir.a.drozdov@gmail.com>
2021-10-05 22:12:47 +0300

    commit from Vladimir

---
+++ src/model/Circle.java
@@ -0,0 +1,16 @@
+package model;
+
+import utils.BtaUtils;
+
+public class Circle {
+    int radius;
+    public Circle(){
+        System.out.println("Random radius ..");
+        radius = BtaUtils.getRandom(20,30);
+    }
+    public double getLenght(){
+        return Math.PI * this.radius;
+    }
+}
+
+
---
+++ src/model/Gender.java
@@ -0,0 +1,8 @@
+package model;
+
+public enum Gender {
+    MAN,
+    WOMAN
+
+}
+
