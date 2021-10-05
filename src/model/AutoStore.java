cf5a75cf559137bbc4739ea4980a36dab2712331
Alex <aleks.aardam1@gmail.com>
2021-10-05 18:04:21 +0300

    added constructor and method in Car model

---
+++ src/model/AutoStore.java
@@ -0,0 +1,22 @@
+package model;
+
+public class AutoStore {
+    private long balance;
+    private Car[] parking = new Car[5];
+
+    public AutoStore(long balance) {
+        this.balance = balance;
+    }
+
+    public Car sellAuto (int parkingPlace){
+
+        //TODO
+        return null;
+    }
+
+    public void buyAuto (Car auto){
+        //TODO
+
+    }
+
+    }
---
+++ src/model/Car.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Car {
+    private int price;
+    private int weight;
+
+    private static final int MIN_PRICE = 80_000;
+    private static final int MAX_PRICE = 400_000;
+
+    public Car (int price, int weight){
+        priceCheck(price);
+        this.price = price;
+        this.weight = weight;
+    }
+
+    private void priceCheck(int price){
+        if (price < MIN_PRICE || price > MAX_PRICE){
+            System.out.println("Car price (" + price + " EURO) is invalid!" +
+                    "Must be in range [" + MIN_PRICE + "," + MAX_PRICE + "]");
+            throw new RuntimeException();
+        }
+    }
+
+    public void setPrice(int price){
+        priceCheck(price);
+        this.price = price;
+    }
+
+}
