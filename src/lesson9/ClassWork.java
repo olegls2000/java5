b0524c6cc7b101b08485298adeb1cac3390d11c8
Vladlen <adment@b1oga.net>
2021-10-06 19:18:05 +0300

    Add more code in AutoStore.java and Car.java +

--- src/lesson9/ClassWork.java
+++ src/lesson9/ClassWork.java
@@ -8,7 +8,13 @@ public class ClassWork {
         Car toyota = new Car(5000, 1500);
         Car honda = new Car(6000, 1500);
         AutoStore autoStore = new AutoStore(5000);
-        AutoStore.addCar(bmw, )
+        AutoStore.addCar(bmw, 0);
+        AutoStore.addCar(toyota, 1);
+        AutoStore.addCar(honda, 2);
+
+        autoStore.sellAuto(0);
+        autoStore.sellAuto(4);
+        autoStore.sellAuto(1);
     }
 
 }
