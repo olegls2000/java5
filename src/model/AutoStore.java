10404f773361d969ec49eeeef8bafcb6b03f8714
Vladlen <adment@b1oga.net>
2021-10-06 19:20:43 +0300

    update AutoStore.java

--- src/model/AutoStore.java
+++ src/model/AutoStore.java
@@ -2,6 +2,7 @@ package model;
 
 public class AutoStore {
     private static final double INTEREST = 1.2;
+
     private long balance;
     private Car[] parking = new Car[5];
 
@@ -9,6 +10,7 @@ public class AutoStore {
         this.balance = balance;
     }
 
+    @Deprecated
     public void addCar(Car car, int place) {
         parking[place] = car;
     }
@@ -17,21 +19,31 @@ public class AutoStore {
         int minValue = 0;
         int maxValue = parking.length - 1;
         if (parkingPlace < minValue || parkingPlace > maxValue) {
-            System.out.println("Parking place is " + parkingPlace + " is invalid. Must be range: [" + minValue + ", " + maxValue + "]");
-        return null;
+            System.out.println("Parking place: " + parkingPlace + " is invalid. " +
+                    "Must be in range: [" +
+                    minValue + ", " + maxValue + "]");
+            return null;
         }
-    }
-
-
-    public Car sellAuto(int parkingPlace) {
         Car carToSale = parking[parkingPlace];
+        if (carToSale == null) {
+            System.out.println("No Car has been found on Parking Place: " + parkingPlace);
+            return null;
+        }
         parking[parkingPlace] = null;
         double income = carToSale.getPrice() * INTEREST;
         balance += income;
+
+        System.out.println("Successfully sold out!!!");
         return carToSale;
     }
 
     public void buyAuto(Car auto) {
         // TODO
+        //check free place, balance, auto not null
+    }
+    public void report(){
+        System.out.println("Balance: " + balance);
+        //TODO add parking state
+        // add toString to Car class
     }
 }
