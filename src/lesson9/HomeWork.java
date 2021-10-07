268cd75ff0cc24b1a69167c1002aea485582267e
macbook <vladimir.a.drozdov@gmail.com>
2021-10-07 21:35:37 +0300

    commit

--- src/lesson9/HomeWork.java
+++ src/lesson9/HomeWork.java
@@ -7,13 +7,13 @@ public class HomeWork {
     public static void main(String[] args) {
         //TODO: rename
 
-        Car BentleyContinental = new Car(350000, 2450);
-        Car LadaBaklazhan = new Car(3200, 1567);
-        Car HyundaiI40 = new Car(24700, 2230);
-        Car ShitAndSticks500 = new Car(3149, 2500);
-        Car LamborghiniVelosiped = new Car(399000, 1201);
-        Car LamborghiniVelosipedRoadster = new Car(400000, 1203);
-        Car LincolnNavigator = new Car(149000, 2460);
+        Car bentleyContinental = new Car(350000, 2450);
+        Car ladaBaklazhan = new Car(3200, 1567);
+        Car hyundaiI40 = new Car(24700, 2230);
+        Car shitAndSticks500 = new Car(3149, 2500);
+        Car lamborghiniVelosiped = new Car(399000, 1201);
+        Car lamborghiniVelosipedRoadster = new Car(400000, 1203);
+        Car lincolnNavigator = new Car(149000, 2460);
 
     }
 
