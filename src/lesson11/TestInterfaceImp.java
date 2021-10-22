91098dcb62094c94c3771a57165e5c6b11821753
oleg.pasichnyk <oleg.pasichnyk@kuehne-nagel.com>
2021-10-22 21:15:50 +0300

    Class Work Anonymous Classes

---
+++ src/lesson11/TestInterfaceImp.java
@@ -0,0 +1,13 @@
+package lesson11;
+
+public class TestInterfaceImp implements TestInterface{
+    @Override
+    public void testMethod() {
+
+    }
+
+    @Override
+    public void testMethod2() {
+
+    }
+}
