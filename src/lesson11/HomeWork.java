8d5ab5a2a99402e0bcc56fe36fc41888d46b37a1
D-pils <d-art@inbox.lv>
2021-10-25 08:44:09 +0300

    Commit from Sergei

---
+++ src/lesson11/HomeWork.java
@@ -0,0 +1,15 @@
+package lesson11;
+
+public class HomeWork {
+	public static int calculateFactorial(int n) {
+		int result = 1;
+		for (int i = 1; i <=n; i++) {
+			result = result*i;
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(calculateFactorial(6));
+	}
+}
