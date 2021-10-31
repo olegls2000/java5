a1442d9800e1261088750de254f72e1cd7d99e9b
macbook <vladimir.a.drozdov@gmail.com>
2021-10-31 19:30:55 +0200

    commit

--- src/lesson12abstractvsinterface/ClassWork.java
+++ src/lesson12abstractvsinterface/ClassWork.java
@@ -1,54 +1,59 @@
 package lesson12abstractvsinterface;
 
-public class ClassWork {
+public class ClassWork {   public static void main(String[] args) {
+    String strPool = "My String";
+    String strPool2 = "My String";
+    String strHeap = new String("My String");
+    String strHeap2 = new String("My String");
 
-        public static void main(String[] args) {
-            String strPool = "My String";
-            String strPool2 = "My String";
-            String strHeap = new String("My String");
-            String strHeap2 = new String("My String");
+    System.out.println(strPool == strPool2);
+    System.out.println(strHeap == strHeap2);
+    System.out.println(strPool.equals(strPool2));
+    System.out.println(strPool.equals(strHeap));
 
-            System.out.println(strPool == strPool2);
-            System.out.println(strHeap == strHeap2);
+    String immutable = "My name is "; // выкидывает на помойку
+    immutable = immutable + "Oleg";
+    System.out.println(immutable);
 
-            System.out.println(strPool.equals(strPool2));
-            System.out.println(strPool.equals(strHeap));
+    StringBuilder stringBuilder = new StringBuilder("My name is "); // так правильнее (1 тип билдера)
+    stringBuilder.append("***");
+    stringBuilder.append(" Oleg ");
+    stringBuilder.append("***");
+    stringBuilder.append("!!!");
+    System.out.println(stringBuilder.toString());
 
-            String immutable = "My name is ";
-            immutable = immutable + "Oleg";
-            System.out.println(immutable);
+    StringBuffer stringBuffer = new StringBuffer("XXX"); // Второй вариант, разница в
+    // методе append и он синхронизирован. Зайти в этот метод может только один поток
+    stringBuffer.append(" --- ");
+    stringBuffer.append(" !!! ");
+    System.out.println(stringBuffer.toString());
 
-            StringBuilder stringBuilder = new StringBuilder("My name is ");
-            stringBuilder.append("Oleg").append("!");
-            System.out.println("Tra lal ala ");
-            stringBuilder.append("!!!!!");
-            System.out.println(stringBuilder.toString());
+    String a = "a"; // сравниваеи регистры
+    String aU = "A";
+    System.out.println(a.equalsIgnoreCase(aU));
 
-            StringBuffer stringBuffer = new StringBuffer("XXX");
-            stringBuffer.append("-");
-            //equals, equals ignorecase, trim(), charAt(int);
-            // indexOf(char); substring(int, int); replace(char, char);
-            // contains(); startWith(char), split(char)
-
-            String a = "tablE";
-            String aU = "Table";
-            System.out.println(a.toLowerCase().equals(aU.toLowerCase()));
-            System.out.println(a.equalsIgnoreCase(aU));
-            String trimExample = "012Some Text           ";
-            trimExample = trimExample.trim();
-            System.out.println(trimExample);
-            System.out.println(trimExample.charAt(2));
-            System.out.println(trimExample.length());
-            System.out.println(trimExample.indexOf('X'));
-            String replaceExample = "Hello Hi";
-            replaceExample = replaceExample.replace('H', 'Y');
-            System.out.println(replaceExample);
-            String splitExample  = "Hello World! I am here.";
-            String[] words = splitExample.split(" ");
-
-            System.out.println(words.length);
-            splitExample.isBlank();
-        }
-    }
+    String aa = "tablE"; // сравниваеи регистры в словах
+    String aUU = "Table";
+    System.out.println(aa.toLowerCase().equals(aUU.toLowerCase()));
+    String trimExamle = " Some Text    ";
+    System.out.println(trimExamle.trim()); // удаляет пробелы мпереди и сзади
+    System.out.println(trimExamle.charAt(2)); // вытаскиевает второй симвл 0, 1, 2
+    System.out.println(trimExamle.length()); // длина строки
+    System.out.println(trimExamle.indexOf('T')); // какая по счёту буква в слове / словах
 
 
+    String replaceExample = "Hello Hi";
+    replaceExample.replace('H', 'Y');
+    System.out.println(replaceExample); // не изменит строку
+
+    String replaceExample2 = "Hello Hi";
+    replaceExample2 = replaceExample2.replace('H', 'Y');
+    System.out.println(replaceExample2); // изменит строку
+
+    String splitExample = "Hello world! I am here.";
+    String[] words = splitExample.split(" ");
+    System.out.println(words.length);
+
+    splitExample.isBlank();
+}
+}
