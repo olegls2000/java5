1d484a5bd4e6d6a8c7d4bbea5814c1ffafb9a2f9
macbook <vladimir.a.drozdov@gmail.com>
2021-10-31 19:31:39 +0200

    commit

--- src/lesson12abstractvsinterface/li.java
+++ src/lesson12abstractvsinterface/li.java
@@ -3,19 +3,19 @@ package lesson12abstractvsinterface;
 import java.io.Serializable;
 import java.util.concurrent.Callable;
 
-    interface Ii extends
-            Serializable,
-            Runnable,
-            Callable {
-        int SOME_FIELD = 7;
+interface Ii extends
+        Serializable,
+        Runnable,
+        Callable {
+    int SOME_FIELD = 7;
 
-        void print();
+    void print();
 
-        default void method1(){
-            System.out.println("from Abstract");
-        }
-
-
+    default void method1() {
+        System.out.println("from Abstract");
     }
+
+
+}
 
 
