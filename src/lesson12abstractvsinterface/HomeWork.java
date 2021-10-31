9a338e0325c96b80ba823ccde47aea0b71ee60f4
macbook <vladimir.a.drozdov@gmail.com>
2021-10-31 20:17:50 +0200

    commit

--- src/lesson12abstractvsinterface/HomeWork.java
+++ src/lesson12abstractvsinterface/HomeWork.java
@@ -7,7 +7,9 @@ public class HomeWork {
     public static class FemaleLogic {
         public static void main(String[] args) {
             Scanner scan = new Scanner(System.in);
+            System.out.println("Please enter a sentence: ");
             String line = scan.nextLine();
+            System.out.println("Thank you! Now i will do the job for you, looser!");
             String longWord = "", shortWord = line;
             for (String s : line.split(" ")) {
                 if (s.length() > longWord.length()) longWord = s;
