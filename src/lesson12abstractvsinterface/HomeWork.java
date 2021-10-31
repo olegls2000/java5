75e875c6a7424fea2ee3bc80dff128d1140f195e
macbook <vladimir.a.drozdov@gmail.com>
2021-10-31 19:42:25 +0200

    commit

---
+++ src/lesson12abstractvsinterface/HomeWork.java
@@ -0,0 +1,20 @@
+package lesson12abstractvsinterface;
+
+import java.util.Scanner;
+
+public class HomeWork {
+
+    public static class FemaleLogic {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            String line = scan.nextLine();
+            String longWord = "", shortWord = line;
+            for (String s : line.split(" ")) {
+                if (s.length() > longWord.length()) longWord = s;
+                if (s.length() < shortWord.length()) shortWord = s;
+            }
+            System.out.println("Longest word: '" + longWord + "'");
+            System.out.println("Shortest word: '" + shortWord + "'");
+        }
+    }
+}
