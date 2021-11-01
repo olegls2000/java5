b090e9e5b6b810ac62b459461f36bceb62214b0d
oleg.pasichnyk <oleg.pasichnyk@kuehne-nagel.com>
2021-11-01 21:17:57 +0200

    Class Work 14

--- src/lesson14/ClassWork.java
+++ src/lesson14/ClassWork.java
@@ -1,10 +1,10 @@
 package lesson14;
 
+import lesson10.homework.Car;
+import lesson10.homework.Manufacturer;
+
 import java.math.BigDecimal;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class ClassWork {
     public static void main(String[] args) {
@@ -30,6 +30,41 @@ public class ClassWork {
         map.put(3, "3");
         Set<Integer> keySet = map.keySet();
         Collection<String> values = map.values();
+        Car car1 = new Car(Manufacturer.TOYOTA);
+        Car car2 = new Car(Manufacturer.BMW);
+        Car car3 = new Car(Manufacturer.HONDA);
+        Car car4 = new Car(Manufacturer.HONDA);
+        Map<Manufacturer, Car> carsMap = new HashMap<>(4);
+        carsMap.put(car1.getManufacturer(), car1);
+        carsMap.put(car2.getManufacturer(), car2);
+        carsMap.put(car3.getManufacturer(), car3);
+        carsMap.put(car4.getManufacturer(), car4);
+
+        Collection<Car> values1 = carsMap.values();
+        Collection<Manufacturer> keys = carsMap.keySet();
+        Collection<Map.Entry<Manufacturer, Car>> entries1 = carsMap.entrySet();
+
+        Car value = carsMap.get(Manufacturer.HONDA);
+        boolean isContain = carsMap.containsValue(car3);
+
+
+        Set<Map.Entry<Manufacturer, Car>> entries = carsMap.entrySet();
+
+        for (Map.Entry<Manufacturer, Car> entry : entries) {
+            Manufacturer key = entry.getKey();
+            Car value1 = entry.getValue();
+            System.out.println("Entry - key: " + entry.getKey());
+            System.out.println("Entry - value: " + entry.getValue());
+        }
+
+        Map<Integer, Student> mapStudent = new TreeMap<>();
+        Student student1 = new Student(18, "Ivan");
+        Student student2 = new Student(21, "Nikolai");
+        mapStudent.put(student1.getAge(), student1);
+        mapStudent.put(student1.getAge(), student2);
+        //Map<Integer, Student> mapStudent = new TreeMap<>();
+
+
 
     }
 }
