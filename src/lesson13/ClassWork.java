af361f6afcf5f3fed69b7a8f00d115da5d5d4f83
Vladlen <adment@b1oga.net>
2021-11-01 17:53:35 +0200

    HomeWork 13

--- src/lesson13/ClassWork.java
+++ src/lesson13/ClassWork.java
@@ -14,9 +14,20 @@ public class ClassWork {
         myList.add("One");
         myList.add("6");
         myList.add("6.99");
+        myList.add("Remove this text");
+        myList.add("Don't remove this text");
         System.out.println(myList.size());
         String element = myList.get(0);
         boolean wasDeleted = myList.remove("6");
-        boolean con = myList.containsAll(Arrays.asList("One", "6"));
+        boolean wasDeleted2 = myList.remove("Remove this text");
+        boolean con = myList.containsAll(Arrays.asList("One", "6", "Remove this text", "Don't remove this text"));
+        System.out.println(myList);
+
+
+        Set<TestClass> setString = new HashSet<>();  // что делает эта часть кода?
+        setString.add(new TestClass(2, "2"));
+        setString.add(new TestClass(2, "2"));
+        System.out.println(setString.size());
+
     }
 }
---
+++ src/lesson13/HomeWork.java
@@ -0,0 +1,74 @@
+package lesson13;
+
+import java.util.*;
+
+public class HomeWork {
+    public static void main(String[] args) {
+        ArrayList<String> color = new ArrayList<String>();
+        ArrayList<String> coloradd = new ArrayList<String>();
+        color.add("Red");
+        color.add("Green");
+        color.add("Blue");
+        color.add("Black");
+        coloradd.add("Ultramarine");
+        coloradd.add("Rose");
+        coloradd.add("Violet");
+        color.add("Yellow");
+        color.add("Purple");
+        color.add("White");
+        color.add("Orange");
+        color.add("Dark blue");
+        color.add(0, "Flash"); // ставим Flash первым в индексе
+        System.out.print(color + "" + coloradd); // как эту чёртову хрень замиксовать со всем списком?
+        String element2 = color.get(7);
+        System.out.println();
+        System.out.println();
+        System.out.println("Вытаскиваем рандомный 8-й элемент: " + element2);
+        Collections.shuffle(color);
+        Collections.shuffle(coloradd);
+        color.remove("Flash"); // в жопу Flash
+        color.add(0, "Flash"); // ставим Flash первым в индексе
+        System.out.println();
+        System.out.println("Random:");
+        System.out.println(color + "" + coloradd);
+        System.out.println();
+        String element = color.get(7);
+        System.out.println("Вытаскиваем рандомный 8-й элемент из реверса: " + element);
+        System.out.println();
+
+        ArrayList<String> land = new ArrayList<String>(); // итератор. В чём отличия от предыдущего?
+        land.add("Slovakia");
+        land.add("Finland");
+        land.add("Norway");
+        land.add("Sweden");
+        land.add("Russia");
+        land.add("Latvia");
+        land.add("Poland");
+        Iterator<String> iter = land.iterator();
+        while (iter.hasNext()) {
+            System.out.println("Country: " + iter.next());
+        }
+        Iterator<String> iterReverse = land.iterator();
+        while (iterReverse.hasNext()) {
+            Collections.reverse(land);
+            System.out.println("Reverse Country: " + iterReverse.next());
+        }
+        if (color.contains("Blue")) {
+            System.out.println("Первый элемент найден");
+    } else {
+        System.out.println("Первый элемент НЕ найден");
+    }
+            if(coloradd.contains("Ultramarine")) {
+            System.out.println("Второй элемент найден");
+        } else {
+            System.out.println("Второй элемент НЕ найден");
+        }
+        if(land.contains("Estonia")) {
+            System.out.println("Страна найдена");
+        } else {
+            System.out.println("Страна НЕ найдена");
+        }
+
+    }
+
+}
