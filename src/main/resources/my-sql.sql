f152c5905de4c88a71ea6e1bd7a4ee5a2807c14c
oleg.pasichnyk <oleg.pasichnyk@kuehne-nagel.com>
2021-11-24 18:36:55 +0200

    Class Work 23

---
+++ src/main/resources/my-sql.sql
@@ -0,0 +1,27 @@
+CREATE TABLE country
+(
+	id SERIAL PRIMARY KEY,
+	name CHARACTER VARYING(30),
+	code INTEGER,
+	discription CHARACTER VARYING(30)
+);
+
+CREATE TABLE city
+(
+	id SERIAL PRIMARY KEY,
+	name CHARACTER VARYING(30),
+	code INTEGER,
+	discription CHARACTER VARYING(30),
+	country_id int not null,
+	constraint FK_COUNTRY foreign KEY (country_id) REFERENCES country (id)
+);
+
+CREATE TABLE citizen
+(
+	id SERIAL PRIMARY KEY,
+	firstName CHARACTER VARYING(30),
+	lastName CHARACTER VARYING(30),
+	birth_day date not null,
+	city_id int,
+	constraint FK_CITY foreign KEY (city_id) REFERENCES city (id)
+);
