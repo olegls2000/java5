f425bccf48f909485ddb3533177cdd3dd8c47d91
Alex <aleks.aardam1@gmail.com>
2021-11-29 18:22:18 +0200

    HomeWork, TriangleCrudRepository error

--- src/main/java/org/bta/lesson/lesson25/Classwork.java
+++ src/main/java/org/bta/lesson/lesson25/Classwork.java
@@ -2,10 +2,13 @@ package org.bta.lesson.lesson25;
 
 import org.bta.lesson.repository.CrudRepository;
 import org.bta.lesson.repository.PointCrudRepository;
+import org.bta.lesson.repository.TriangleCrudRepository;
 
 public class Classwork {
     public static void main(String[] args) {
         CrudRepository pointRepository = new PointCrudRepository();
         System.out.println(pointRepository.selectAll());
+        CrudRepository triangleRepository = new TriangleCrudRepository();
+        System.out.println(triangleRepository.selectAll());
     }
 }
