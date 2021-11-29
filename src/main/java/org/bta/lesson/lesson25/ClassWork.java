7a380b7559a5893eecddf9cfd586ddc47b1b1fd7
macbook <vladimir.a.drozdov@gmail.com>
2021-11-29 21:22:12 +0200

    commit

--- src/main/java/org/bta/lesson/lesson25/ClassWork.java
+++ src/main/java/org/bta/lesson/lesson25/ClassWork.java
@@ -5,11 +5,19 @@ import org.bta.lesson.model.Triangle;
 import org.bta.lesson.reposytory.CrudRepository;
 import org.bta.lesson.reposytory.PointCrudRepository;
 import org.bta.lesson.reposytory.TriangleCrudRepository;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ClassWork {
     public static void main(String[] args) {
-        CrudRepository pointRepository = new PointCrudRepository();
-        CrudRepository triangleRepository = new TriangleCrudRepository();
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+
+        CrudRepository pointRepository = context.getBean(PointCrudRepository.class);
+        CrudRepository pointRepository2 = context.getBean(PointCrudRepository.class);
+        CrudRepository triangleRepository = context.getBean(TriangleCrudRepository.class);
+        CrudRepository triangleRepository2 = context.getBean(TriangleCrudRepository.class);
+
         System.out.println(pointRepository.selectAll());
 
         Point a = new Point(88, 99);
